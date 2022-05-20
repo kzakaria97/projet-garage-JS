b7d1a47d5f5558bbecef725e13ad6ea2f82c6619
kzakaria97 <kastit.zakaria@hotmail.com>
2022-05-20 12:27:11 +0200

    inspection of auto issues (for,if,else) and payement

--- public/js/garage.js
+++ public/js/garage.js
@@ -40,3 +40,37 @@ let moteur = new Devis("moteur en panne", "moteur neuf", 800);
 let huile = new Devis("huile à moteur vide", "huile neuf", 100);
 
 let pieces = [rouleur, pneu, feu, moteur, huile];
+
+let i=0
+for (i; i < salle_attente.personnes.length ; i+2) {
+    bureau.personnes.unshift(salle_attente.personnes[i]);
+    console.log(`${salle_attente.personnes[i].nom} est rentré dans le bureau.`);
+    salle_attente.personnes.splice(0,1);
+    console.log(`Après inspection ${garagiste.nom} trouve le problème du véicule et fais un devis pour l'inspection`);
+    garagiste.argent += 45;
+    bureau.personnes[i].devis = pieces[i];
+    bureau.personnes[i].argent -= 45;
+    console.log(`${bureau.personnes[0].nom} a payé 45e pour le devis, il a encore un total de ${bureau.personnes[0].argent}e pour faire sa réparation.`)
+    console.log(`le garagiste jean a donc gagné pour le moment : ${garagiste.argent}€ sur ce client.`);
+    console.log(`Les clients qui attendent leur tour pour réparer leur voiture sont`);
+    console.table(salle_attente.personnes);
+    caisse.personnes.push(bureau.personnes[0]);
+    bureau.personnes.shift();
+    i;
+}
+console.log("----------------------------------------------------------------------------------------------------");
+
+console.log(`les cinqs clients sont entrain d'attendre ${garagiste.nom} à la caisse`);
+console.table(caisse.personnes);
+console.log(`Après l'inspection de jean voici les problèmes qu'il a diagnostiqué sur les véhicule des clients`)
+console.log("----------------------------------------------------------------------------------------------------");
+console.table(rouleur);
+console.table(pneu);
+console.table(feu);
+console.table(moteur);
+console.table(huile);
+console.log("----------------------------------------------------------------------------------------------------");
+
+
+
+export {Lieux, caisse, salle_attente, bureau, Devis, pieces};
